66705963165dd922727ba1b6ac11b2e24f05f8d6
Kholmumin <xolmomin@gmail.com>
2025-10-25 11:57:25 +0500

    add task

--- apps/models/category.py
+++ apps/models/category.py
@@ -3,11 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, editable=False)
 
     def __str__(self):
         return self.name
-
-
-
-
